9289118394db9e64cbbe033e610cfb6e9edb2902
antonivegaochoa <tonivegaochoa@gmail.com>
2021-05-02 15:14:25 -0400

    day 45 & 46, finished challenge

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -86,57 +86,134 @@ struct Arc: InsettableShape {
     }
 }
 
-struct Flower: Shape {
-    // How much to move this petal away from the center
-    var petalOffset: Double = -20
+//struct Flower: Shape {
+//    // How much to move this petal away from the center
+//    var petalOffset: Double = -20
+//
+//    // How wide to make each petal
+//    var petalWidth: Double = 100
+//
+//    func path(in rect: CGRect) -> Path {
+//        // The path that will hold all petals
+//        var path = Path()
+//
+//        // Count from 0 up to pi * 2, moving up pi / 8 each time
+//        for number in stride(from: 0, to: CGFloat.pi * 2, by: CGFloat.pi / 8) {
+//            // rotate the petal by the current value of our loop
+//            let rotation = CGAffineTransform(rotationAngle: number)
+//
+//            // move the petal to be at the center of our view
+//            let position = rotation.concatenating(CGAffineTransform(translationX: rect.width / 2, y: rect.height / 2))
+//
+//            // create a path for this petal using our properties plus a fixed Y and height
+//            let originalPetal = Path(ellipseIn: CGRect(x: CGFloat(petalOffset), y: 0, width: CGFloat(petalWidth), height: rect.width / 2))
+//
+//            // apply our rotation/position transformation to the petal
+//            let rotatedPetal = originalPetal.applying(position)
+//
+//            // add it to our main path
+//            path.addPath(rotatedPetal)
+//        }
+//
+//        // now send the main path back
+//        return path
+//    }
+//}
 
-    // How wide to make each petal
-    var petalWidth: Double = 100
-
+struct Arrow: Shape {
     func path(in rect: CGRect) -> Path {
-        // The path that will hold all petals
         var path = Path()
-
-        // Count from 0 up to pi * 2, moving up pi / 8 each time
-        for number in stride(from: 0, to: CGFloat.pi * 2, by: CGFloat.pi / 8) {
-            // rotate the petal by the current value of our loop
-            let rotation = CGAffineTransform(rotationAngle: number)
-
-            // move the petal to be at the center of our view
-            let position = rotation.concatenating(CGAffineTransform(translationX: rect.width / 2, y: rect.height / 2))
-
-            // create a path for this petal using our properties plus a fixed Y and height
-            let originalPetal = Path(ellipseIn: CGRect(x: CGFloat(petalOffset), y: 0, width: CGFloat(petalWidth), height: rect.width / 2))
-
-            // apply our rotation/position transformation to the petal
-            let rotatedPetal = originalPetal.applying(position)
-
-            // add it to our main path
-            path.addPath(rotatedPetal)
-        }
-
-        // now send the main path back
+        
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY * 0.5))
+        path.addLine(to: CGPoint(x: rect.midX * 0.5, y: rect.maxY * 0.5))
+        path.addLine(to: CGPoint(x: rect.midX * 0.5, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.midX * 1.5, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.midX * 1.5, y: rect.maxY * 0.5))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY * 0.5))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY * 0.5))
+        
         return path
     }
 }
 
+struct ColorCyclingCircle: View {
+    var amount = 0.0
+    var steps = 100
+
+    var body: some View {
+        ZStack {
+            ForEach(0..<steps) { value in
+                Rectangle()
+                    .inset(by: CGFloat(value))
+//                    .strokeBorder(self.color(for: value, brightness: 1), lineWidth: 2)
+                    .strokeBorder(LinearGradient(gradient: Gradient(colors: [
+                        self.color(for: value, brightness: 1),
+                        self.color(for: value, brightness: 0.5)
+                    ]), startPoint: .top, endPoint: .bottom), lineWidth: 2)
+            }
+        }
+        .drawingGroup()
+    }
+
+    func color(for value: Int, brightness: Double) -> Color {
+        var targetHue = Double(value) / Double(self.steps) + self.amount
+
+        if targetHue > 1 {
+            targetHue -= 1
+        }
+
+        return Color(hue: targetHue, saturation: 1, brightness: brightness)
+    }
+}
+
 struct ContentView: View {
-    @State private var petalOffset = -20.0
-    @State private var petalWidth = 100.0
+    @State private var dash: CGFloat = 20
+    @State private var width: CGFloat = 5
+    @State private var colorCycle = 0.0
+    @State private var dashPhase: CGFloat = 40
+    @State private var rotationAmount: Double = 0
+
+
 
     var body: some View {
         VStack {
-            Flower(petalOffset: petalOffset, petalWidth: petalWidth)
-//                .stroke(Color.red, lineWidth: 1)
-                .fill(Color.red, style: FillStyle(eoFill: true))
-
-            Text("Offset")
-            Slider(value: $petalOffset, in: -40...40)
-                .padding([.horizontal, .bottom])
-
-            Text("Width")
-            Slider(value: $petalWidth, in: 0...100)
-                .padding(.horizontal)
+            Stepper("Border Size", onIncrement: {
+                withAnimation(Animation.easeOut) {
+                    width *= 2
+                    dash *= 2
+                    rotationAmount += 90
+                }
+                withAnimation(Animation.linear(duration: 2.75).repeatForever(autoreverses: false)) {
+                    dashPhase += (dash*2)
+                }
+            }, onDecrement: {
+                withAnimation {
+                    width /= 2
+                    dash /= 2
+                    rotationAmount -= 90
+                }
+                withAnimation(Animation.linear(duration: 2.75).repeatForever(autoreverses: false)) {
+                    dashPhase += (dash*2)
+                }
+            })
+            .padding()
+            
+            Spacer()
+            
+            Arrow()
+                .stroke(Color.secondary, style: StrokeStyle(lineWidth: width, lineCap: .round, dash: [dash], dashPhase: dashPhase))
+                .frame(width: 100, height: 200)
+                .rotationEffect(.degrees(rotationAmount))
+            
+            Spacer()
+            
+        }
+        .onAppear {
+            withAnimation(Animation.linear(duration: 2.75).repeatForever(autoreverses: false)) {
+                dashPhase += (dash*2)
+            }
         }
     }
 }
